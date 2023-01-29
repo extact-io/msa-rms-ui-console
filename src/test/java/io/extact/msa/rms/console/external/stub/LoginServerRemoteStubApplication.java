41984c12ff9d1a32e9cb96af7ddc13b2b28c5afc
ozytso <ozytso@gmail.com>
2023-01-29 22:29:17 +0900

    refactor: update around openapi

--- src/test/java/io/extact/msa/rms/console/external/stub/LoginServerRemoteStubApplication.java
+++ src/test/java/io/extact/msa/rms/console/external/stub/LoginServerRemoteStubApplication.java
@@ -1,16 +1,32 @@
 package io.extact.msa.rms.console.external.stub;
 
+import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
 import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
 
 import io.extact.msa.rms.platform.core.jwt.provider.JwtProvideResponseFilter;
-import io.extact.msa.rms.platform.fw.webapi.RmsApplication;
+import io.extact.msa.rms.platform.fw.webapi.RmsBaseApplications;
 
 @ApplicationPath("auth")
-public class LoginServerRemoteStubApplication extends RmsApplication {
+public class LoginServerRemoteStubApplication extends Application {
+
     @Override
-    protected Set<Class<?>> getWebApiClasses() {
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new LinkedHashSet<>();
+        classes.addAll(RmsBaseApplications.CLASSES);
+        classes.addAll(getWebApiClasses());
+        return classes;
+    }
+
+    @Override
+    public Map<String, Object> getProperties() {
+        return RmsBaseApplications.PROPERTIES;
+    }
+
+    private Set<Class<?>> getWebApiClasses() {
         return Set.of(
                     JwtProvideResponseFilter.class,
                     LoginServerRemoteStub.class
--- src/test/java/io/extact/msa/rms/console/external/stub/RmsServerRemoteStubApplication.java
+++ src/test/java/io/extact/msa/rms/console/external/stub/RmsServerRemoteStubApplication.java
@@ -1,22 +1,38 @@
 package io.extact.msa.rms.console.external.stub;
 
+import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
 import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
 
 import org.eclipse.microprofile.auth.LoginConfig;
 
 import io.extact.msa.rms.platform.fw.login.LoginUserFromJwtRequestFilter;
-import io.extact.msa.rms.platform.fw.webapi.RmsApplication;
+import io.extact.msa.rms.platform.fw.webapi.RmsBaseApplications;
 
 @LoginConfig(authMethod = "MP-JWT")
 @ApplicationPath("api")
-public class RmsServerRemoteStubApplication extends RmsApplication {
+public class RmsServerRemoteStubApplication extends Application {
+
     @Override
-    protected Set<Class<?>> getWebApiClasses() {
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new LinkedHashSet<>();
+        classes.addAll(RmsBaseApplications.CLASSES);
+        classes.addAll(getWebApiClasses());
+        return classes;
+    }
+
+    @Override
+    public Map<String, Object> getProperties() {
+        return RmsBaseApplications.PROPERTIES;
+    }
+
+    private Set<Class<?>> getWebApiClasses() {
         return Set.of(
-                LoginUserFromJwtRequestFilter.class,
-                RmsServerRemoteStub.class
+                    LoginUserFromJwtRequestFilter.class,
+                    RmsServerRemoteStub.class
                 );
     }
 }
